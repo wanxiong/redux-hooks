de316e4ffbce56669e50ce0e31e0fd4752881c73
wanxiong <wanx@zhinanmao.com>
2021-01-08 17:57:38 +0800

    xinzeng

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { memo } from 'react';
 import { renderRoutes } from 'react-router-config';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import Footer from '@/components/footer';
@@ -12,14 +12,14 @@ import routes from './router'
 function App() {
   return (
     <Provider store={store}>
-      <HashRouter>
+      <Route>
         <div>
           <Header />
           {renderRoutes(routes)}
           <Player />
           <Footer />
         </div>
-      </HashRouter>
+      </Route>
     </Provider>
   );
 }
